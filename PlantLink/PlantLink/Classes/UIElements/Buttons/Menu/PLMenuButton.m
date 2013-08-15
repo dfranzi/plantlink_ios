6058a8d5d643c82e5a1b3610bd88cd55e76253a9
Zealous Amoeba <support@zealousamoeba.com>
2013-08-15 00:46:07 -0500

    updated icons and new menu button design and ux

--- PlantLink/PlantLink/Classes/UIElements/Buttons/Menu/PLMenuButton.m
+++ PlantLink/PlantLink/Classes/UIElements/Buttons/Menu/PLMenuButton.m
@@ -17,11 +17,20 @@
         [self.layer setCornerRadius:8.0f];
         [self.layer setMasksToBounds:YES];
         
-        UIImage *backgroundUp = [GeneralMethods imageWithColor:Color_MenuButton_Up andSize:self.frame.size];
-        UIImage *backgroundDown = [GeneralMethods imageWithColor:Color_MenuButton_Down andSize:self.frame.size];
-    
-        [self setBackgroundImage:backgroundUp forState:UIControlStateNormal];
-        [self setBackgroundImage:backgroundDown forState:UIControlStateSelected];
+        CGSize size = CGSizeMake(self.frame.size.width, self.frame.size.height-3);
+        UIImage *foreground = [GeneralMethods imageWithColor:Color_MenuButton_Up andSize:size];
+        foreground = [GeneralMethods image:foreground withCornerRadius:8.0f];
+        UIImage *background = [GeneralMethods imageWithColor:Color_MenuButton_Down andSize:self.frame.size];
+        
+        UIGraphicsBeginImageContext( self.frame.size );
+        [background drawInRect:CGRectMake(0,0,self.frame.size.width,self.frame.size.height)];
+        [foreground drawInRect:CGRectMake(0,0,size.width,size.height) blendMode:kCGBlendModeOverlay alpha:1.0];
+        UIImage *newForeground = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+
+        UIImage *touchDownBackground = [GeneralMethods imageWithColor:Color_MenuButton_Up andSize:self.frame.size];
+        [self setBackgroundImage:newForeground forState:UIControlStateNormal];
+        [self setBackgroundImage:touchDownBackground forState:UIControlStateHighlighted];
     }
     return self;
 }
